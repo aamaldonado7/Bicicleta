3c7f8e80041b1041704d322b53a5e761dfcf63c4
aamaldonado <aamaldonado@9Y4TY21.caracolknits.com>
2018-07-12 09:01:46 -0600

    Mi proyecto Bicicleta

---
+++ src/pkg3/bicicleta/Bicicleta.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg3.bicicleta;
+
+/**
+ *
+ * @author aamaldonado
+ */
+public class Bicicleta {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     int cadenciaInicial = 0; 
+     int velocidadInicial = 0; 
+     int engranajeInicial = 0;   
+            
+        Bicicleta1 bicicleta = new Bicicleta1(cadenciaInicial, velocidadInicial, engranajeInicial);
+        bicicleta.Acelerar(15);
+    }
+    
+}
